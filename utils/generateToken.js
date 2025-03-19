34d6c8ae3388277cf1209b0f127fcc46e19f2d4c
Gangadhar-mudhiraj <142510369+Gangadhar-mudhiraj@users.noreply.github.com>
2025-03-19 12:27:38 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,14 +1,12 @@
-import jwt from 'jsonwebtoken';
-
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
 
   res.cookie('jwt', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'strict',
+    httpOnly: true,  // Ensures the cookie can't be accessed by JavaScript
+    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production (HTTPS)
+    sameSite: 'None', // Allows cookies to be sent in cross-origin requests
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
